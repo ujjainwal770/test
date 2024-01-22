3054b9baa2de8fd27b92c2e8304f1d40581c8f39
Rinku Ujjainwal <rinku.ujjainwal@inadev.com>
2024-01-22 14:25:32 +0530

    ..

--- parameters.groovy
+++ parameters.groovy
@@ -4,4 +4,4 @@ env.unitTest="false"
 env.deployment=true
 env.namespace="decisionservice-pre-prod-ns"
 env.ApplicationName="cosigh"
-env.RepositoryName="303294497261.dkr.ecr.us-east-2.amazonaws.com/cosign"
+env.RepositoryName="618187721717.dkr.ecr.us-east-2.amazonaws.com/cosign"
