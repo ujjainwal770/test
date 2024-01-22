e6be9e9c74a15182695d9573aa5987e0e0c6f4f9
Joker <50817370+ujjainwal770@users.noreply.github.com>
2024-01-22 18:36:52 +0530

    Update parameters.groovy

--- parameters.groovy
+++ parameters.groovy
@@ -3,5 +3,5 @@ env.framework="maven"
 env.unitTest="false"
 env.deployment=true
 env.namespace="decisionservice-pre-prod-ns"
-env.ApplicationName="cosigh"
+env.ApplicationName="cosign"
 env.RepositoryName="618187721717.dkr.ecr.us-east-2.amazonaws.com/cosign"
