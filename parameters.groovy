bcda9dc9432269dda9b1a1a7a1dc7582938ac555
Rinku Ujjainwal <rinku.ujjainwal@inadev.com>
2024-01-22 14:24:06 +0530

    first commit

---
+++ parameters.groovy
@@ -0,0 +1,7 @@
+env.build_version="0.0.1"
+env.framework="maven"
+env.unitTest="false"
+env.deployment=true
+env.namespace="decisionservice-pre-prod-ns"
+env.ApplicationName="cosigh"
+env.RepositoryName="303294497261.dkr.ecr.us-east-2.amazonaws.com/cosign"
